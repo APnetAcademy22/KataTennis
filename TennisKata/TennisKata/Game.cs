b0b3cb8580865df91144e04039e2ff9f683a23b8
APnetAcademy22 <104847257+APnetAcademy22@users.noreply.github.com>
2022-05-11 15:57:33 +0200

    Tests Completed

---
+++ TennisKata/TennisKata/Game.cs
@@ -0,0 +1,17 @@
+﻿namespace TennisKata
+{
+    public class Game
+    {
+        private string Player1 { get; set; }
+        private string Player2 { get; set; }
+        public Game ( string player1, string player2)
+        {
+            Player1 = player1;
+            Player2 = player2;
+        }
+        public string ResultToString(int pointsPlayer1, int pointsPlayer2)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
